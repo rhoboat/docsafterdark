468fbedebf80c3aa6459e0e61ca5a1682e83e58a
Raymond Wang <me@waymondrang.com>
2023-04-03 11:07:56 -0700

    update kix_app.js payload

--- src/chrome/kix_app.js
+++ src/chrome/kix_app.js
@@ -20,3 +20,68 @@ function Cmg(a, b, c, d, e) {
   c && $T(c, a);
   a.Ha(!0);
 }
+
+/**
+ * Dynamically created elements, such as the word count modal below,
+ * can be modified by overwriting its respective function definition
+ */
+function u8g(a) {
+  var b = a.tsc,
+    c = a.nTb,
+    d = a.rsc,
+    e = a.kTb,
+    f = a.qsc,
+    g = a.jTb,
+    k = a.ssc,
+    l = a.lTb;
+  a = a.iuc;
+  var n = !YS(f, 0);
+  var p = Hve + T("kix-documentmetricsdialog-table") + Ute;
+  l &&
+    (p += v8g(
+      qw(DP),
+      k,
+      l,
+      n,
+      rw(MFe + T(KYe) + " " + T(IYe) + '"'),
+      rw(MFe + T(KYe) + " " + T(MYe) + '"')
+    ));
+  p += v8g(
+    qw("Words"),
+    b,
+    c,
+    n,
+    rw(MFe + T(KYe) + " " + T(IYe) + '"'),
+    rw(MFe + T(KYe) + " " + T(MYe) + '"')
+  );
+  p += v8g(
+    qw("Characters"),
+    f,
+    g,
+    n,
+    rw(MFe + T(KYe) + " " + T(IYe) + '"'),
+    rw(MFe + T(KYe) + " " + T(MYe) + '"')
+  );
+  p += v8g(
+    qw("Characters excluding spaces"),
+    d,
+    e,
+    n,
+    rw(MFe + (a ? T(KYe) : T(JYe)) + " " + T(IYe) + '"'),
+    rw(MFe + (a ? T(KYe) : T(JYe)) + " " + T(MYe) + '"')
+  );
+  p +=
+    mve +
+    (a
+      ? m +
+        T("kix-documentmetricsdialog-control") +
+        " " +
+        T(JYe) +
+        " " +
+        T(ui) +
+        PO +
+        T(a) +
+        ea
+      : "");
+  return S(p);
+}
